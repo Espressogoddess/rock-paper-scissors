18157fb7544a2cb75f955be602596ccb3a7f7dc0
Amber Shipley <ashipley2@me.com>
2023-01-11 15:56:15 -0800

    Reorganize constructor and rewrite startNewGame function to only change game type

--- game.js
+++ game.js
@@ -1,17 +1,16 @@
 class Game {
     constructor() {
+        var astronaut = new Player('AstroKitty', 'src/cat astronaut.png', 'Astronaut kitty icon', 0,);
         var computer = new Player('Computer', 'src/computer.png', 'Computer icon', 0);
-        var astronaut = new Player('AstroKitty', 'src/cat astronaut.png', 'Astronaut kitty icon', 0);
         this.players = [astronaut, computer];
+        this.type;
     }
-    startNewGame() {
-        for (var i = 0; i < this.players.length; i++) {
-            this.players[i].wins = 0;
-        }
+    startNewGame(gameType) {
+        this.type = gameType;
     }
+
 }
 
 // A way to keep track of the data for the game board
-// A way to keep track of the selected game type
 // A way to check the Game’s board data for win conditions
 // A way to detect when a game is a draw (no one has won)
