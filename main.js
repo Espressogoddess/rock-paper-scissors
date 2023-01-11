1a32edb41af593ef889802cae5cde2893cbca3ed
Amber Shipley <ashipley2@me.com>
2023-01-11 15:49:25 -0800

    Add event listeners, handlers, and functions upon game choice and add functions to show and hide elements

--- main.js
+++ main.js
@@ -6,18 +6,55 @@ var astroWins = document.querySelector('#astro-wins');
 var compWins = document.querySelector('#comp-wins');
 var astroToken = document.querySelector('#astro-icon');
 var compToken = document.querySelector('#comp-icon');
+var changeGameButton = document.querySelector('#change-game-button');
+var classicGame = document.querySelector('#classic-game');
+var spaceGame = document.querySelector('#space-game');
+var chooseGame = document.querySelector('#choose-game-section');
+var chooseFighterSection = document.querySelector('#choose-fighter-section')
+var classicFighters = document.querySelector('#classic-fighter-section');
+var spaceFighters = document.querySelector('#space-fighter-section');
 
 window.addEventListener('load', function () {
-    showPlayerInfo(currentGame)
+    displayPlayerInfo(currentGame)
 })
 
-function showPlayerInfo(currentGame) {
+classicGame.addEventListener('click', function() {
+    currentGame.startNewGame('classic');
+    hideElement(chooseGame);
+    showElement(chooseFighterSection);
+    showElement(classicFighters)
+    displayGame();
+})
+
+spaceGame.addEventListener('click', function() {
+    currentGame.startNewGame('space');
+    hideElement(chooseGame);
+    showElement(chooseFighterSection);
+    showElement(spaceFighters);
+    displayGame();
+})
+
+function displayPlayerInfo(currentGame) {
     astroName.innerText = currentGame.players[0].name;
     astroWins.innerText = currentGame.players[0].wins;
-    astroToken.src = currentGame.players[0].tokenSource
+    astroToken.src = currentGame.players[0].tokenSource;
     astroToken.alt = currentGame.players[0].altText;
-    compToken.src = currentGame.players[1].tokenSource
     compName.innerText = currentGame.players[1].name;
     compWins.innerText = currentGame.players[1].wins;
+    compToken.src = currentGame.players[1].tokenSource;
     compToken.alt = currentGame.players[1].altText;
 }
+
+function hideElement(element) {
+    element.classList.add('hidden');
+}
+
+function showElement(element) {
+    element.classList.remove('hidden');
+}
+
+function displayGame() {
+
+
+}
+//remove hidden from change game button after choosing game
