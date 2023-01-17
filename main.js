76ad4bf699eae68a14cd0e25d43cefdda39ffb31
Amber Shipley <ashipley2@me.com>
2023-01-17 14:06:48 -0800

    Add utility classes, organize css, refactor js to be dryer

--- main.js
+++ main.js
@@ -22,30 +22,19 @@ var timerID2;
 
 /*----------------Event Listeners-----------------*/
 window.addEventListener('load', function () {
-    displayPlayerInfo(currentGame);
+    renderPlayerInfo(currentGame);
 })
 
-classicGame.addEventListener('click', function() {
-    currentGame.startNewGame('classic');
-    hideElement(chooseGame);
-    showElement(chooseFighterSection);
-    showElement(classicFighters);
-    hideElement(spaceFighters);
-    showElement(changeGameButton);
-    renderPlayerFighterToken();
+
+classicGame.addEventListener('click', function () {
+    setGameType('classic');
 })
 
-spaceGame.addEventListener('click', function() {
-    currentGame.startNewGame('space');
-    hideElement(chooseGame);
-    showElement(chooseFighterSection);
-    showElement(spaceFighters);
-    hideElement(classicFighters);
-    showElement(changeGameButton);
-    renderPlayerFighterToken();
+spaceGame.addEventListener('click', function () {
+    setGameType('space');
 })
 
-changeGameButton.addEventListener('click', function() {
+changeGameButton.addEventListener('click', function () {
     currentGame.startNewGame(null);
     hideElement(chooseFighterSection);
     showElement(chooseGame);
@@ -54,52 +43,18 @@ changeGameButton.addEventListener('click', function() {
     clearTimeout(timerID2);
 })
 
-classicFighters.addEventListener('click', function(event) {
+classicFighters.addEventListener('click', function (event) {
     var fighterName = event.target.dataset.classicFighter;
-    if(!fighterName) {
-        return;
-    }
-    currentGame.players[0].takeTurn(fighterName, currentGame.classicFighters);
-    currentGame.players[1].takeTurn(null, currentGame.classicFighters);
-    renderPlayerFighterToken();
-    currentGame.determineWinner();
-    displayPlayerInfo(currentGame);
-    timerID1 = setTimeout(function() {
-        hideElement(chooseFighterSection);
-        renderResultsSection()
-    }, 400);   
-    timerID2 = setTimeout(function() {
-        hideElement(resultSection);
-        currentGame.startNewGame("classic");
-        renderPlayerFighterToken();
-        showElement(chooseFighterSection);
-    }, 2000);
+    processResults(fighterName, currentGame.classicFighters, 'classic');
 })
 
-spaceFighters.addEventListener('click', function(event) {
+spaceFighters.addEventListener('click', function (event) {
     var fighterName = event.target.dataset.spaceFighter;
-    if(!fighterName) {
-        return;
-    }
-    currentGame.players[0].takeTurn(fighterName, currentGame.spaceFighters);
-    currentGame.players[1].takeTurn(null, currentGame.spaceFighters);
-    renderPlayerFighterToken();
-    currentGame.determineWinner();
-    displayPlayerInfo(currentGame);
-    timerID1 = setTimeout(function() {
-        hideElement(chooseFighterSection);
-        renderResultsSection()
-    }, 400);   
-    timerID2 = setTimeout(function() {
-        hideElement(resultSection);
-        currentGame.startNewGame("space");
-        renderPlayerFighterToken();
-        showElement(chooseFighterSection);
-    }, 2000);
+    processResults(fighterName, currentGame.spaceFighters, 'space');
 })
 
-/*------------------Functions---------------*/
-function displayPlayerInfo(currentGame) {
+/*------------------------Functions-----------------------*/
+function renderPlayerInfo(currentGame) {
     astroName.innerText = currentGame.players[0].name;
     astroWins.innerText = currentGame.players[0].wins;
     astroToken.src = currentGame.players[0].tokenSource;
@@ -110,14 +65,20 @@ function displayPlayerInfo(currentGame) {
     compToken.alt = currentGame.players[1].altText;
 }
 
-function hideElement(element) {
-    element.classList.add('hidden');
+function setGameType(gameType) {
+    currentGame.startNewGame(gameType);
+    if (gameType === 'classic') {
+        showElement(classicFighters);
+        hideElement(spaceFighters);
+    } else {
+        showElement(spaceFighters);
+        hideElement(classicFighters);
+    }
+    hideElement(chooseGame);
+    showElement(chooseFighterSection);
+    showElement(changeGameButton);
+    renderPlayerFighterToken();
 }
-
-function showElement(element) {
-    element.classList.remove('hidden');
-}
-
 
 function renderPlayerFighterToken() {
     for (var i = 0; i < choiceIcons.length; i++) {
@@ -128,16 +89,35 @@ function renderPlayerFighterToken() {
             if (currentFighter && (isClassicChoice || isSpaceChoice)) {
                 choiceIcons[i].src = currentGame.players[0].tokenSource
                 choiceIcons[i].style.opacity = "1";
-            }   
-        }
-        else {
+            }
+        } else {
             choiceIcons[i].style.opacity = '0'
         }
     }
 }
 
+function processResults(fighterName, fighters, gameType) {
+    if (!fighterName) {
+        return;
+    }
+    currentGame.players[0].takeTurn(fighterName, fighters);
+    currentGame.players[1].takeTurn(null, fighters);
+    renderPlayerFighterToken();
+    currentGame.determineWinner();
+    renderPlayerInfo(currentGame);
+    timerID1 = setTimeout(function () {
+        hideElement(chooseFighterSection);
+        renderResultsSection()
+    }, 400);
+    timerID2 = setTimeout(function () {
+        hideElement(resultSection);
+        currentGame.startNewGame(gameType);
+        renderPlayerFighterToken();
+        showElement(chooseFighterSection);
+    }, 2000);
+}
+
 function renderResultsSection() {
-    resultSection.innerHTML ='';
     var winnerTitle;
     if (currentGame.winner === 'draw') {
         winnerTitle = 'Draw!'
@@ -146,24 +126,31 @@ function renderResultsSection() {
     }
     var player0 = currentGame.players[0]
     var player1 = currentGame.players[1]
+    var player0class = '';
+    var player1class = '';
+    if (currentGame.winner === 'Computer') {
+        player0class = 'fighter-loser';
+    } else if (currentGame.winner === 'AstroKitty') {
+        player1class = 'fighter-loser';
+    }
     resultSection.innerHTML = `
-    <h2 id="won-this-round">${winnerTitle}</h2>
-      <div class="fighter-result">
-        <img class="large-icon" id="player0" alt="${player0.currentFighter.altText}" src="${player0.currentFighter.imageSource}">
-        <img class="large-icon" id="player1" alt="${player1.currentFighter.altText}" src="${player1.currentFighter.imageSource}">
-      </div>
-      <div class="choice-icon-container-2">
-        <img class="small-icon" alt="${player0.altText}" src="${player0.tokenSource}">
-        <img class="small-icon" alt="${player1.altText}" src="${player1.tokenSource}">
-      </div>
-          `
-          if (currentGame.winner === 'Computer') {
-            var astroFighter = document.querySelector('#player0');
-            astroFighter.style.opacity = ".5";
-          } 
-          else if (currentGame.winner === 'AstroKitty') {
-            var compFighter = document.querySelector('#player1');
-            compFighter.style.opacity = ".5"
-          }
-      showElement(resultSection);
+        <h2>${winnerTitle}</h2>
+        <div class="fighter-result">
+            <img class="large-icon ${player0class}" alt="${player0.currentFighter.altText}" src="${player0.currentFighter.imageSource}">
+            <img class="large-icon ${player1class}" alt="${player1.currentFighter.altText}" src="${player1.currentFighter.imageSource}">
+        </div>
+        <div class="choice-icon-container-2 d-flex">
+            <img class="small-icon" alt="${player0.altText}" src="${player0.tokenSource}">
+            <img class="small-icon" alt="${player1.altText}" src="${player1.tokenSource}">
+        </div>
+    `;
+    showElement(resultSection);
+}
+
+function hideElement(element) {
+    element.classList.add('hidden');
+}
+
+function showElement(element) {
+    element.classList.remove('hidden');
 }
